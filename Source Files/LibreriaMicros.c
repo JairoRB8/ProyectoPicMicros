7009cef34f405425152997951c683efe860227b6
David-Solorzano <david.jose.0503@gmail.com>
2020-06-22 10:16:54 -0600

    Rename LibreriaMicros.c to Source Files/LibreriaMicros.c

--- Source Files/LibreriaMicros.c
+++ Source Files/LibreriaMicros.c
@@ -18,7 +18,7 @@ void USART_Init(long baud){
     
     //Tx
     TXSTAbits.TX9 = 0; //8 bits
-    TXSTAbits.TXEN = 1; //habilitar transmisión
+    TXSTAbits.TXEN = 1; //habilitar transmisiÃ³n
     
     //Rx
     RCSTAbits.RC9 = 0; //8 bits
